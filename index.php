6b5965e2b5f71fb7d005361e06d95ae18c530e0b
metamn <bartus.csongor@gmail.com>
2019-06-30 14:10:21 +0300

    Working on icons

--- index.php
+++ index.php
@@ -49,5 +49,5 @@
         To begin the development, run `npm run wpstart` or `yarn wpstart`.
         To create a production bundle, use `npm run wpbuild` or `yarn wpbuild`.
     -->
-    <script src="/react-wp/wp-content/themes/inu-v2-a/static/js/runtime~main.c9856d86.js"></script><script src="/react-wp/wp-content/themes/inu-v2-a/static/js/1.fea4431f.chunk.js"></script><script src="/react-wp/wp-content/themes/inu-v2-a/static/js/main.84006ee3.chunk.js"></script></body>
+    <script src="/react-wp/wp-content/themes/inu-v2-a/static/js/runtime~main.c9856d86.js"></script><script src="/react-wp/wp-content/themes/inu-v2-a/static/js/1.fea4431f.chunk.js"></script><script src="/react-wp/wp-content/themes/inu-v2-a/static/js/main.f8b95b0c.chunk.js"></script></body>
 </html>
--- react-src/src/components/Icon/Icon.js
+++ react-src/src/components/Icon/Icon.js
@@ -2,32 +2,71 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
+import { useTheme } from "../../hooks";
+
+/**
+ * Defines the prop types of the component
+ * @type {Object}
+ */
 const propTypes = {
+  /**
+   * The size multiplier
+   * The width, height of the icon will be `var(--lem) * size`
+   * @type {[type]}
+   */
   size: PropTypes.number,
+  /**
+   * The icon status
+   * @type {[type]}
+   */
+  status: PropTypes.oneOf(["active", "inactive", "hidden"]),
+  /**
+   * The icon itself. Preferably in SVG format.
+   * @type {[type]}
+   */
   children: PropTypes.any.isRequired
 };
 
+/**
+ * Defines the default props
+ * @type {Object}
+ */
 const defaultProps = {
   size: 1.5,
+  status: "active",
   children: ""
 };
 
-const className = "icon";
-
+/**
+ * Displays the icon container
+ * @type {[type]}
+ */
 const Element = styled("div")(props => ({
   width: `calc(var(--lem) * ${props.size})`,
   height: `calc(var(--lem) * ${props.size})`,
+
+  visibility: props.status === "hidden" ? "hidden" : "visible",
+  color:
+    props.status === "active"
+      ? props.theme.colors.text
+      : props.theme.colors.inactive,
+  cursor: props.status === "active" ? "pointer" : "default",
+
   svg: {
     fontSize: `calc(var(--lem) * ${props.size})`
   }
 }));
 
+/**
+ * Displays an icon
+ * @param Object props The component properties
+ */
 const Icon = props => {
   const { children } = props;
-  console.log(props.size);
+  const [theme] = useTheme();
 
   return (
-    <Element className={className} {...props}>
+    <Element className="icon" theme={theme} {...props}>
       {children}
     </Element>
   );
