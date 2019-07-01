aa3a4e01132591fe0d11b8d1f7a629e7626f6fcc
metamn <bartus.csongor@gmail.com>
2019-07-01 20:52:33 +0300

    Storybook ready for use

--- react-src/.storybook/config.js
+++ react-src/.storybook/config.js
@@ -1,4 +1,28 @@
-import { configure } from "@storybook/react";
+import { configure, addParameters, addDecorator } from "@storybook/react";
+import theme from "./theme";
+
+// Console imports ... more complicated than the rest
+import { withConsole } from "@storybook/addon-console";
+import { setConsoleOptions } from "@storybook/addon-console";
+import "@storybook/addon-console";
+
+// General settings
+addParameters({
+  options: {
+    theme: theme,
+    panelPosition: "right"
+  },
+  backgrounds: [
+    { name: "silver", value: "silver", default: true },
+    { name: "white", value: "#fff" }
+  ]
+});
+
+// Console settings
+setConsoleOptions({
+  panelExclude: []
+});
+addDecorator((storyFn, context) => withConsole()(storyFn)(context));
 
 // Load stories into the sidebar
 const req = require.context("../src", true, /.stories.js$/);
---
+++ react-src/.storybook/theme.js
@@ -0,0 +1,7 @@
+import { create } from "@storybook/theming";
+
+export default create({
+  base: "dark",
+  brandTitle: "Inu Chivu",
+  brandUrl: "http://inu.ro"
+});
