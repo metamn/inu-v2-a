27efe9ff86108b0acaff364777a500d0d27911a0
metamn <bartus.csongor@gmail.com>
2019-07-01 20:37:18 +0300

    Adding stories

---
+++ react-src/src/components/Categories/Categories.stories.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo-hooks";
+import Categories from "./Categories";
+import markdownNotes from "./Categories.md";
+
+const client = new ApolloClient({
+  uri: "http://localhost/react-wp/graphql"
+});
+
+storiesOf("Components/Categories", module).add(
+  "Overview",
+  () => (
+    <ApolloProvider client={client}>
+      <Categories />
+    </ApolloProvider>
+  ),
+  {
+    notes: { markdown: markdownNotes }
+  }
+);
